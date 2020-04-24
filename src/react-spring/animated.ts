1ba0274ec83b33c130490755bafb378fd0e40423
colder <colder@vitarn.com>
2020-04-24 14:20:27 +0800

    feat: 添加 react-spring

---
+++ src/react-spring/animated.ts
@@ -0,0 +1,30 @@
+import { View, Text, Image } from '@tarojs/components'
+import { Globals, animated as animatedWeb, AnimatedComponent } from '@react-spring/web'
+import { ElementType } from '@react-spring/shared'
+
+type CreateAnimated = <T extends ElementType>(wrappedComponent: T) => AnimatedComponent<T>
+
+declare type TaroElements = typeof elements
+declare type TaroComponents = {
+    [P in keyof TaroElements]: AnimatedComponent<TaroElements[P]>
+}
+
+const elements = {
+    View,
+    Text,
+    Image,
+}
+
+const animated = (animatedWeb as unknown) as CreateAnimated & TaroComponents
+
+// 其他平台采用的 extendAnimated 方式在小程序里无效. 我们采用直接赋值.
+for (const key of Object.keys(elements)) {
+    const element = elements[key]
+    animated[key] = animated(element)
+}
+
+Globals.assign({
+    defaultElement: View,
+})
+
+export { animated as a, animated }
---
+++ src/react-spring/applyAnimatedValues.ts
@@ -0,0 +1,39 @@
+import { Globals } from '@react-spring/web'
+
+import { toDashed } from '../utils'
+
+type Instance = HTMLDivElement & { style: any; [key: string]: any }
+
+const applyAnimatedValuesWeb = Globals.applyAnimatedValues
+/**
+ * TaroElement 设置 style 属性比设置字符串 cssText 理论上性能更高, 这种方式可以跳过 react setState, 但是逃不过小程序 setData.
+ *
+ * 在碰到兼容问题前, 暂时交由 spring web 处理 style.
+ *
+ * 1. TaroElement.style[name] = value
+ * 2. TaroElement.setAttribute('style', styleString)
+ */
+export const applyAnimatedValues = (
+    instance: Instance,
+    props: {
+        style: React.CSSProperties
+        [attr: string]: any
+    }
+) => {
+    if (!instance.nodeType || !instance.setAttribute) {
+        return false
+    }
+
+    const { style = {}, ...attributes } = props!
+
+    const styleString = Object.keys(style).reduce((str, key) => {
+        const val = style[key]
+        return val ? `${str}${toDashed(key)}: ${val};` : str
+    }, '')
+
+    if (styleString) {
+        instance.setAttribute('style', styleString)
+    }
+
+    applyAnimatedValuesWeb(instance, attributes)
+}
